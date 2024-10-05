b86f8aa4f49a66861dfabbb33030d8665087a272
User <mariajosegaspar95@gmail.com>
2024-10-04 22:02:45 -0500

    Imolementacion de consumo de servicio con WC

--- src/main/java/pe/edu/cibertec/patitas_frontend_wc_a/config/WebClientConfig.java
+++ src/main/java/pe/edu/cibertec/patitas_frontend_wc_a/config/WebClientConfig.java
@@ -1,14 +1,30 @@
 package pe.edu.cibertec.patitas_frontend_wc_a.config;
 
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class WebClientConfig {
 
+    HttpClient httpClient = HttpClient.create()
+            .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000) // Timeout para la conexión
+            .responseTimeout(Duration.ofSeconds(5)) // Timeout para la respuesta
+            .doOnConnected(conn -> conn.addHandlerLast(new ReadTimeoutHandler(10, TimeUnit.SECONDS)) // Timeout de lectura
+            );
+
     @Bean
-    public WebClient webClientAutenticacion(WebClient.Builder builder){
-        return builder.build();
+    public WebClient webClientAuthentication(WebClient.Builder builder) {
+        return builder
+                .baseUrl("http://localhost:8081/autenticacion")
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .build();
     }
 }
--- src/main/java/pe/edu/cibertec/patitas_frontend_wc_a/controller/LoginController.java
+++ src/main/java/pe/edu/cibertec/patitas_frontend_wc_a/controller/LoginController.java
@@ -11,6 +11,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 import pe.edu.cibertec.patitas_frontend_wc_a.dto.LoginRequestDTO;
 import pe.edu.cibertec.patitas_frontend_wc_a.dto.LoginResponseDTO;
 import pe.edu.cibertec.patitas_frontend_wc_a.viewmodel.LoginModel;
+import reactor.core.publisher.Mono;
 
 @Controller
 @RequestMapping("/login")
@@ -47,7 +48,14 @@ public class LoginController {
 
             // Invocar servicio de autenticación
             LoginRequestDTO loginRequestDTO = new LoginRequestDTO(tipoDocumento, numeroDocumento, password);
-            LoginResponseDTO loginResponseDTO = webClientAutenticacion.postForObject("/login", loginRequestDTO, LoginResponseDTO.class);
+            Mono<LoginResponseDTO> monoLoginResponseDTO = webClientAutenticacion.post()
+                    .uri("/login")
+                    .body(Mono.just(loginRequestDTO), LoginRequestDTO.class)
+                    .retrieve()
+                    .bodyToMono(LoginResponseDTO.class);
+
+            //Recuprar resultado modo bloqueante(sincrono)
+            LoginResponseDTO loginResponseDTO = monoLoginResponseDTO.block();
 
             if (loginResponseDTO.codigo().equals("00")){
 
